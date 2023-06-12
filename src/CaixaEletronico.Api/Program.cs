dd5ca92de22a50fe7f439be931259d1708915892
Matheus-Morais <matheus2992morais@gmail.com>
2023-06-12 09:37:07 -0300

    Alterado AddScoped para AddSingleton

--- src/CaixaEletronico.Api/Program.cs
+++ src/CaixaEletronico.Api/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IConta, Conta>();
+builder.Services.AddSingleton<IConta, Conta>();
 
 var app = builder.Build();
 
